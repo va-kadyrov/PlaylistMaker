742e76b3b8a723380641ab71dde38472185c72a0
Василий <va.kadyrov@yandex.ru>
2024-09-21 22:51:51 +0300

    spring 8 commit 2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,14 +2,14 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val btnBack = findViewById<Button>(R.id.btn_back)
+        val btnBack = findViewById<Toolbar>(R.id.btn_back)
         btnBack.setOnClickListener{
             this.finish()
         }
