5855f956c8014c8dea099a44fafa7a7713249766
Василий <va.kadyrov@yandex.ru>
2024-10-01 16:09:18 +0300

    spring 8 commit 2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,67 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.search_et)
+        val btnClear = findViewById<ImageView>(R.id.search_btn_clear)
+        val btnBack = findViewById<Toolbar>(R.id.search_tb_back)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) btnClear.visibility = View.INVISIBLE
+                else btnClear.visibility = View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) inputText = s.toString()
+            }
+        }
+
+        btnClear.setOnClickListener{
+            inputEditText.setText("")
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+        }
+
+        btnBack.setOnClickListener{
+            finish()
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, inputText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        inputText = savedInstanceState.getString(INPUT_TEXT, "")
+    }
+
+    companion object {
+        const val INPUT_TEXT = "INPUT_TEXT"
+    }
+
+    var inputText = ""
+
 }
