8b38bbaf202b6e51f421a6c846bed33b94156e9f
va-kadyrov <va.kadyrov@yandex.ru>
2025-05-05 16:06:10 +0300

    sprint 12 commit 1

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -4,4 +4,5 @@ class Track (
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Long,
-    val artworkUrl100: String)
+    val artworkUrl100: String,
+    val trackId: Double)
