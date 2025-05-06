89252959b1226c5082f511420372ec9844f3a2d6
va-kadyrov <va.kadyrov@yandex.ru>
2025-05-06 10:14:56 +0300

    sprint 13 commit 1

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,14 @@
 package com.example.playlistmaker
 
+import java.util.Date
+
 class Track (
     val trackName: String,
+    val collectionName: String,
     val artistName: String,
     val trackTimeMillis: Long,
+    val releaseDate: Date,
+    val primaryGenreName: String,
+    val country: String,
     val artworkUrl100: String,
     val trackId: Long)
