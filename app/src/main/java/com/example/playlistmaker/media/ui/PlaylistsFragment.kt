45db27f5dcf0ab62f97c6d170993d962219e747c
va-kadyrov <va.kadyrov@yandex.ru>
2026-03-15 20:44:32 +0300

    спринт 27 PlaylistsScreen

--- app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
@@ -32,34 +32,9 @@ import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import java.util.Date
 import kotlin.getValue
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PlaylistsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlaylistsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
 
     val viewModel by activityViewModel<PlaylistsViewModel>()
-//    private lateinit var binding: FragmentPlaylistsBinding
-
-//    val playlists = mutableListOf<Playlist>()
-//    val playlistsAdapter = PlaylistsAdapter(playlists)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-    }
 
     override fun onResume(){
         super.onResume()
@@ -70,9 +45,7 @@ class PlaylistsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-//        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
-//        return binding.root
+
         return ComposeView(requireContext()).apply{
             setContent {
                 PlaylistsScreen(
@@ -89,95 +62,8 @@ class PlaylistsFragment : Fragment() {
                 )
             }
         }
-
-
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-//        val playlistsRecView = binding.playlistsRecView
-//        val btnNewPlaylist = binding.btnNewPlaylist
-//
-//        playlistsRecView.layoutManager = GridLayoutManager(requireActivity(), 2)
-//        playlistsRecView.adapter = playlistsAdapter
-//
-//        btnNewPlaylist.setOnClickListener {
-//            findNavController().navigate(R.id.action_mediaFragment_to_newPlaylistFragment)
-//        }
-//
-//        viewModel.observePlaylistsState().observe(viewLifecycleOwner) {playlistsStateObserver(it) }
-
-    }
-
-//    class PlaylistsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-//        private val imageView: ImageView = itemView.findViewById(R.id.playlist_item_img)
-//        private val nameView: TextView = itemView.findViewById(R.id.playlist_item_name)
-//        private val tracksView: TextView = itemView.findViewById(R.id.playlist_item_tracks)
-
-//        fun bind(playlist: Playlist) {
-//            nameView.text = playlist.name
-//            tracksView.text = trackCountsToString(playlist.trackCounts)
-//            Glide.with(itemView)
-//                .load(playlist.filePath)
-//                .transform(CenterCrop(), RoundedCorners(32))
-//                .placeholder(R.drawable.album_cover_empty)
-//                .into(imageView)
-//        }
-//
-//        private fun trackCountsToString(trackCounts: Int): String {
-//            when {
-//                trackCounts in 11..20 -> return "$trackCounts треков"
-//                trackCounts % 10 == 1 -> return "$trackCounts трек"
-//                trackCounts % 10 in 2..4 -> return "$trackCounts трека"
-//                else -> return "$trackCounts треков"
-//            }
-//        }
-//    }
-
-//    inner class PlaylistsAdapter(private val items: List<Playlist>): RecyclerView.Adapter<PlaylistsViewHolder> () {
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistsViewHolder {
-//            val view = LayoutInflater.from(parent.context)
-//                .inflate(R.layout.playlist_recycler_layout, parent, false)
-//            return PlaylistsViewHolder(view)
-//        }
-//
-//        override fun onBindViewHolder(holder: PlaylistsViewHolder, position: Int) {
-//            holder.bind(items[position])
-//            holder.itemView.setOnClickListener {
-//                findNavController().navigate(
-//                    R.id.action_mediaFragment_to_playlistInfoFragment,
-//                    PlaylistInfoFragment.createArgs(items[position].id))
-//            }
-//        }
-//
-//        override fun getItemCount(): Int {
-//            return items.size
-//        }
-//    }
-//
-//    private fun playlistsStateObserver(it: PlaylistsState) {
-//        playlists.clear()
-//        when {
-//            it.isEmpty -> showNothing()
-//            else -> {
-//                playlists.addAll(it.playlists)
-//                playlistsAdapter.notifyDataSetChanged()
-//                showPlaylists()
-//            }
-//        }
-//    }
-//
-//    fun showPlaylists(){
-//        binding.playlistsRecView.visibility = VISIBLE
-//        binding.playlistsEmptyFrame.visibility = GONE
-//    }
-//
-//    fun showNothing(){
-//        binding.playlistsRecView.visibility = GONE
-//        binding.playlistsEmptyFrame.visibility = VISIBLE
-//    }
-//
     companion object {
         fun newInstance() = PlaylistsFragment()
     }
--- app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsScreen.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsScreen.kt
@@ -30,10 +30,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
@@ -49,10 +51,42 @@ fun PlaylistsScreen(
 
     val playlistsState by observePlaylistsState.observeAsState()
 
-    if (playlistsState?.isEmpty?:true) {
-        NoPlaylists(addNewPlaylist)
-    } else {
-        PlaylistsList(playlistsState!!.playlists, openPlaylist)
+    PlaylistsScreen1(playlistsState!!, addNewPlaylist, openPlaylist)
+}
+
+@Composable
+fun PlaylistsScreen1(
+    playlistsState: PlaylistsState,
+    addNewPlaylist: () -> Unit,
+    openPlaylist: (Long) -> Unit
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        ButtonNewPlaylist(addNewPlaylist)
+
+        if (playlistsState?.isEmpty ?: true) {
+            NoPlaylists()
+        } else {
+            PlaylistsList(playlistsState!!.playlists, openPlaylist)
+        }
+    }
+}
+
+@Composable
+private fun ButtonNewPlaylist(onClick: () -> Unit) {
+    Button(
+        modifier = Modifier.padding(top = 24.dp),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            contentColor = colorResource(R.color.settings_back),
+            containerColor = colorResource(R.color.search_pl_text)
+        ),
+    ) {
+        Text(
+            text = stringResource(R.string.new_playlist),
+            fontSize = dimensionResource(R.dimen.little_text).value.sp,
+            fontFamily = FontFamily(Font(R.font.ys_display_medium)),
+        )
     }
 }
 
@@ -60,13 +94,14 @@ fun PlaylistsScreen(
 fun PlaylistsList(playlists: List<Playlist>, openPlaylist: (Long) -> Unit ){
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
         verticalArrangement =  Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ){
         items(playlists.size) { index -> PlaylistRow(playlists[index], openPlaylist) }
     }
 }
+
 
 @Composable
 @OptIn(ExperimentalGlideComposeApi::class)
@@ -86,54 +121,55 @@ fun PlaylistRow(playlist: Playlist, openPlaylist: (Long) -> Unit){
         Text(
             text = playlist.name,
             fontSize = 12.sp,
+            fontFamily = FontFamily(Font(R.font.ys_display_medium)),
+            color = colorResource(R.color.almostblack_white),
             maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
             modifier = Modifier.padding(top = 4.dp)
         )
         Text(
-            text = playlist.description,
+            text = trackCountsToString(playlist.trackCounts),
             fontSize = 12.sp,
+            fontFamily = FontFamily(Font(R.font.ys_display_medium)),
+            color = colorResource(R.color.almostblack_white),
             maxLines = 1,
-            modifier = Modifier.padding(top = 4.dp)
         )
     }
 }
 
+
+private fun trackCountsToString(trackCounts: Int): String {
+    when {
+        trackCounts in 11..20 -> return "$trackCounts треков"
+        trackCounts % 10 == 1 -> return "$trackCounts трек"
+        trackCounts % 10 in 2..4 -> return "$trackCounts трека"
+        else -> return "$trackCounts треков"
+    }
+}
+
+
 @Composable
-private fun NoPlaylists(newPlaylist: () -> Unit){
+private fun NoPlaylists(){
     Column(Modifier
-        .padding(vertical = 12.dp)
+        .padding(vertical = 46.dp)
         .fillMaxWidth()) {
-        Button(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            onClick = newPlaylist,
-            colors = ButtonDefaults.buttonColors(
-                contentColor = colorResource(R.color.settings_back),
-                containerColor = colorResource(R.color.search_pl_text)
-            ),
-        ) {
-            Text(
-                text = stringResource(R.string.new_playlist),
-                fontSize = dimensionResource(R.dimen.little_text).value.sp,
-                fontFamily = FontFamily(Font(R.font.ys_display_medium)),
-            )
-        }
         Image(
             modifier = Modifier
                 .size(120.dp, 120.dp)
-                .padding(bottom = 16.dp)
                 .align(Alignment.CenterHorizontally),
             painter = painterResource(R.drawable.nothing_found),
             contentDescription = "nothing foung",
             contentScale = ContentScale.FillWidth
         )
-        Text(modifier = Modifier.align(Alignment.CenterHorizontally),
-            text = stringResource(R.string.nothing_found),
+        Text(modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+            text = stringResource(R.string.no_playlists),
             textAlign = TextAlign.Center,
             fontSize = dimensionResource(R.dimen.big_text).value.sp,
             fontFamily = FontFamily(Font(R.font.ys_display_medium)),
             color = colorResource(R.color.search_pl_text),
-
-            )
+        )
     }
 }
 
@@ -143,27 +179,29 @@ private fun NoPlaylists(newPlaylist: () -> Unit){
 @Composable
 @Preview
 fun NoPlaylistsPreview(){
-    NoPlaylists({})
+    PlaylistsScreen1(PlaylistsState(true,dummyPlaylists()), {}, {})
 }
+
 
 @Composable
 @Preview
 fun PlaylistsPreview(){
-    PlaylistsList(dummyPlaylists(), {})
+    PlaylistsScreen1(PlaylistsState(false,dummyPlaylists()), {}, {})
 }
+
 
 @Composable
 private fun dummyPlaylists():List<Playlist>{
     return listOf<Playlist>(
-        Playlist(1001L, "Серебро", "Серебро", "",
+        Playlist(1001L, "Серебро Серебро Серебро Серебро ", "Серебро", "",
             emptyList<Long>().toMutableList(), 0, 10),
         Playlist(1002L, "Варвара", "Ага", "",
-            emptyList<Long>().toMutableList(), 0, 20),
+            emptyList<Long>().toMutableList(), 11, 20),
         Playlist(1003L, "Варвара", "Ага", "",
-            emptyList<Long>().toMutableList(), 0, 30),
+            emptyList<Long>().toMutableList(), 2, 30),
         Playlist(1004L, "Варвара", "Ага", "",
-            emptyList<Long>().toMutableList(), 0, 40),
+            emptyList<Long>().toMutableList(), 21, 40),
         Playlist(1005L, "Варвара", "Ага", "",
-            emptyList<Long>().toMutableList(), 0, 50),
+            emptyList<Long>().toMutableList(), 3, 50),
     )
 }
